5feef6d6b471202c4ae0846d116c0926e995a0a5
zerebom <ice.choco.pudding.kokoro@gmail.comm>
2020-12-10 07:08:07 +0000

    Add mock codes.

---
+++ src/factory/dataset.py
@@ -0,0 +1,13 @@
+
+import os
+
+import torch
+from torch import nn
+from torch.nn import functional as F
+from torch.utils.data import DataLoader, random_split
+from torchvision.datasets import MNIST
+from torchvision import transforms
+import pytorch_lightning as pl
+from pytorch_lightning.metrics.functional import accuracy
+from pl_bolts.datasets import DummyDataset
+
